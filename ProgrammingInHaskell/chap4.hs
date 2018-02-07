9e89da196971f14c21ee65377c5b3e7efce27a61
Doug Swisher <big.swish@gmail.com>
2018-02-06 20:04:49 -0600

    Ex 4.2

--- ProgrammingInHaskell/chap4.hs
+++ ProgrammingInHaskell/chap4.hs
@@ -3,3 +3,11 @@
 halve xs = ((take n xs), (drop n xs))
            where n = length xs  `div` 2
 
+-- Exercise 2
+third1 xs = head (tail (tail xs))
+
+third2 xs = xs !! 2
+
+third3 (x:y:z:_) = z
+
+
