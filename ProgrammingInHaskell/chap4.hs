0a6b0b15b2803f1ea1848060939e3be885d7c72c
Doug Swisher <big.swish@gmail.com>
2018-02-07 07:22:02 -0600

    Ex 4.3

--- ProgrammingInHaskell/chap4.hs
+++ ProgrammingInHaskell/chap4.hs
@@ -10,4 +10,13 @@ third2 xs = xs !! 2
 
 third3 (x:y:z:_) = z
 
+-- Exercise 3
+safetail :: [a] -> [a]
+-- safetail xs = if null xs then [] else tail xs
+
+-- safetail xs | null xs = []
+--             | otherwise = tail xs
+
+safetail [] = []
+safetail (x:xs) = xs
 
